3ad1f43a0fe229477f82c2e5bbab12a10390eb18
seanwasere youtube <seanwasere@gmail.com>
2018-07-14 08:16:55 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Seans-AngularJS-NodeJS-MongoDB-Boilerplate
+# Seans AngularJS <-> NodeJS <-> MongoDB Boilerplate
+
+Also comes with testing framemork.
+
+To install
+`git clone ...`
+
+`npm install`
+
+`npm start`
+
+## Developing
+
+If you are developing the code,
+you will also need to run 
+`gulp watch`
+This will,
+- detect changes in the source files, 
+- run tests on the files using mocha and chai,
+- compile them from ES6 to Javascript using babel, 
+- concatenate the js using gulp-concat,
+- and minify the js using gulp-uglify,
+- and copy the new scripts and html to the public `www` folder
+
+
+
---
+++ server.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/seanwasere');
+
+var Cat = require('./models/cat.js');
+
+var app = express();
+var port = process.env.PORT || 8080; 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(express.static(path.join(__dirname, 'www')));
+
+var router = express.Router();
+router.route('/cats')
+    .post(function (req, res) {
+        var cat = new Cat();
+        cat.name = req.body.name;
+        cat.save(function (err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Cat created!' });
+        });
+    })
+    .get(function (req, res) {
+        Cat.find(function (err, cats) {
+            if (err)
+                res.send(err);
+
+            res.json(cats);
+        });
+    });
+
+router.route('/cats/:_id')
+    .put(function (req, res) {
+        console.dir(req.body);
+        Cat.findByIdAndUpdate(req.params._id, req.body, function (err, post) {
+           
+            if (err)
+                res.send(err);
+            res.json(post);
+        });
+    })
+    .delete(function (req, res) {
+        Cat.remove({
+            _id: req.params._id
+        }, function (err, bear) {
+            if (err)
+                res.send(err);
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+
+app.use('/api', router);
+
+app.listen(port);
+
+console.log('nodejs server started : \nhttp://127.0.0.1:' + port);
+
